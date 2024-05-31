4df26ca2634e7bf97ee66c1f73dd714e3f120cd2
ITSTIME <88642524+ITSTIME1@users.noreply.github.com>
2024-05-31 21:56:33 +0900

    build : rebase commit to maintain oneline commit

---
+++ presentation/src/main/java/com/spring_greens/presentation/PresentationApplication.java
@@ -0,0 +1,13 @@
+package com.spring_greens.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentationApplication.class, args);
+	}
+
+}
